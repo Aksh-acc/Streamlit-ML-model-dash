12a90c43ea3cd0537dd56951fa41c420fe8cd097
Aksh-acc <akshmodia21@gmail.com>
2025-01-18 11:56:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 st.title('Machine Learning prediction dash')
 st.info('This is a machine learing model app where you can get the insights from the data and get the predictions along with')
-df = pd.read_csv("iris.csv")
+df = pd.read_csv(r"iris.csv")
 with st.expander("data"):
   st.write(df)
