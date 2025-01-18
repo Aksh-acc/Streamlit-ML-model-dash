17720fb00c15160241129fa9196c95184a5c34d8
Aksh-acc <akshmodia21@gmail.com>
2025-01-18 11:55:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 st.title('Machine Learning prediction dash')
 st.info('This is a machine learing model app where you can get the insights from the data and get the predictions along with')
 df = pd.read_csv("iris.csv")
