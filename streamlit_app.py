3be35a6c5ce180a87ec8cb958d74f2595e779021
Aksh-acc <akshmodia21@gmail.com>
2025-01-18 12:15:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,9 @@ st.info('This is a machine learing model app where you can get the insights from
 df = pd.read_csv('https://raw.githubusercontent.com/Aksh-acc/Streamlit-ML-model-dash/refs/heads/master/Iris.csv')
 with st.expander("data"):
   st.write(df)
+st.write('**X vlaue**')
 X = df.drop('Species' ,axis =1)
 X
+st.write('**Y vlaue**')
+y = df.Species
+y
