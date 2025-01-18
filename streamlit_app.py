44a28d4076a807e1320052c97a18c98d69a0a013
Aksh-acc <akshmodia21@gmail.com>
2025-01-18 12:17:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,11 @@ st.title('Machine Learning prediction dash')
 st.info('This is a machine learing model app where you can get the insights from the data and get the predictions along with')
 df = pd.read_csv('https://raw.githubusercontent.com/Aksh-acc/Streamlit-ML-model-dash/refs/heads/master/Iris.csv')
 with st.expander("data"):
+  st.write('**raw data**')
   st.write(df)
-st.write('**X vlaue**')
-X = df.drop('Species' ,axis =1)
-X
-st.write('**Y vlaue**')
-y = df.Species
-y
+  st.write('**X value**')
+  X = df.drop('Species' ,axis =1)
+  X
+  st.write('**Y value**')
+  y = df.Species
+  y
